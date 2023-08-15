e6bf686293f5bcb004d9d5206f57ae97e44dad3f
icanurpramanti <icanurpramanti2@gmail.com>
2023-08-15 17:55:43 +0700

    sidebar commit

--- resources/views/admin/dashboard/layout/sidebar.blade.php
+++ resources/views/admin/dashboard/layout/sidebar.blade.php
@@ -1,5 +1,4 @@
 <ul class="nav">
-
   <li class="{{ Request::is('home') ? 'active' : ''}}">
     <a href="/home">
       <i class="nc-icon nc-shop"></i>
@@ -7,7 +6,6 @@
     </a>
   </li>
 
-  <li>
   <li class="{{ Request::is('kategori') ? 'active' : ''}}">
     <a href="/kategori">
       <i class="nc-icon nc-single-copy-04"></i>
@@ -15,15 +13,13 @@
     </a>
   </li>
 
-  <li>
   <li class="{{ Request::is('satuan') ? 'active' : ''}}">
     <a href="/satuan">
       <i class="nc-icon nc-app"></i>
-      <p>Data Satuan </p>
+      <p>Data Satuan</p>
     </a>
   </li>
 
-  <li>
   <li class="{{ Request::is('produk') ? 'active' : ''}}">
     <a href="/produk">
       <i class="nc-icon nc-tag-content"></i>
@@ -31,7 +27,6 @@
     </a>
   </li>
 
-  <li>
   <li class="{{ Request::is('supplier') ? 'active' : ''}}">
     <a href="/supplier">
       <i class="nc-icon nc-user-run"></i>
@@ -39,9 +34,6 @@
     </a>
   </li>
 
-
-
-  <li>
   <li class="{{ Request::is('pembelian') ? 'active' : ''}}">
     <a href="/pembelian">
       <i class="nc-icon nc-bag-16"></i>
@@ -49,38 +41,35 @@
     </a>
   </li>
 
-  <li>
-  <li class="{{ Request::is('penjualan') ? 'active' : ''}}">
+  <li class="{{ Request::is('penjualan') || Request::routeIs('transaksi.baru') ? 'active' : ''}}">
     <a href="/penjualan">
       <i class="nc-icon nc-money-coins"></i>
       <span>Penjualan</span>
     </a>
   </li>
 
-  <li>
+  <li class="{{ Request::routeIs('transaksi.baru') ? 'active' : ''}}">
     <a href="{{ route('transaksi.baru') }}">
       <i class="nc-icon nc-cart-simple"></i>
       <span>Transaksi Baru</span>
     </a>
   </li>
 
-
-  <li>
+  <li class="{{ Request::routeIs('laporanpembelian.index') ? 'active' : ''}}">
     <a href="{{ route('laporanpembelian.index') }}">
       <i class="nc-icon nc-paper"></i>
       <span>Laporan Pembelian</span>
     </a>
   </li>
 
-  <li>
+  <li class="{{ Request::routeIs('laporanpenjualan.index') ? 'active' : ''}}">
     <a href="{{ route('laporanpenjualan.index') }}">
       <i class="nc-icon nc-paper"></i>
       <span>Laporan Penjualan</span>
     </a>
   </li>
 
-
-  <li>
+  <li class="{{ Request::routeIs('laporan.index') ? 'active' : ''}}">
     <a href="{{ route('laporan.index') }}">
       <i class="nc-icon nc-paper"></i>
       <span>Laporan Pendapatan</span>
@@ -93,6 +82,4 @@
       <p>Kelola User</p>
     </a>
   </li>
-
-
-
+</ul>
--- resources/views/kasir/dashboard/layout2/sidebar.blade.php
+++ resources/views/kasir/dashboard/layout2/sidebar.blade.php
@@ -1,24 +1,22 @@
 <ul class="nav">
-    <li class="{{ Request::is('homee') ? 'active' : ''}}">
-      <a href="/homee">
-        <i class="nc-icon nc-shop"></i>
-        <p>Dashboard</p>
-      </a>
-    </li>
+  <li class="{{ Request::is('homee') ? 'active' : ''}}">
+    <a href="/homee">
+      <i class="nc-icon nc-shop"></i>
+      <p>Dashboard</p>
+    </a>
+  </li>
 
-    <li>
-    <li class="{{ Request::is('penjualankasir') ? 'active' : ''}}">
-      <a href="/penjualankasir">
-        <i class="nc-icon nc-money-coins"></i>
-        <span>Penjualan</span>
-      </a>
-    </li>
+  <li class="{{ Request::is('penjualankasir') || Request::routeIs('transaksikasir.baru') ? 'active' : ''}}">
+    <a href="/penjualankasir">
+      <i class="nc-icon nc-money-coins"></i>
+      <span>Penjualan</span>
+    </a>
+  </li>
 
-    <li>
-      <a href="{{ route('transaksikasir.baru') }}">
-        <i class="nc-icon nc-cart-simple"></i>
-        <span>Transaksi Baru</span>
-      </a>
-    </li>
-
-  </ul>
+  <li class="{{ Request::routeIs('transaksikasir.baru') ? 'active' : ''}}">
+    <a href="{{ route('transaksikasir.baru') }}">
+      <i class="nc-icon nc-cart-simple"></i>
+      <span>Transaksi Baru</span>
+    </a>
+  </li>
+</ul>
